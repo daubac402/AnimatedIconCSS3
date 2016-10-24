63a49c0a8e75cdae10cedd09ec3c9712d7ce4579
theanh <theanh@ilovex.co.jp>
2016-10-24 16:47:53 +0900

    update facebook Like API

--- index.php
+++ index.php
@@ -1,16 +1,53 @@
 <link rel="stylesheet" type="text/css" href="css/global.css">
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-19534192-5', 'auto');
-  ga('send', 'pageview');
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-19534192-5', 'auto');
+ga('send', 'pageview');
 </script>
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '330208494007688',
+        xfbml: true,
+        version: 'v2.8'
+    });
+};
 
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
 <title>Animated Icons</title>
 <h1>Animated Icons</h1>
-
+<h2>
+    <div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true"
+  layout="button_count"
+  colorscheme="dark"
+  >
+</div>
+</h2>
 <div class="icon sun-shower">
     <div class="cloud"></div>
     <div class="sun">
@@ -18,7 +55,6 @@
     </div>
     <div class="rain"></div>
 </div>
-
 <div class="icon thunder-storm">
     <div class="cloud"></div>
     <div class="lightning">
@@ -26,19 +62,16 @@
         <div class="bolt"></div>
     </div>
 </div>
-
 <div class="icon cloudy">
     <div class="cloud"></div>
     <div class="cloud"></div>
 </div>
-
 <div class="icon eclipse">
     <div class="moon_eclipse"></div>
     <div class="sun sun-eclipse">
         <div class="rays"></div>
     </div>
 </div>
-
 <div class="icon flurries">
     <div class="cloud"></div>
     <div class="snow">
@@ -46,25 +79,21 @@
         <div class="flake"></div>
     </div>
 </div>
-
 <div class="icon sunny">
     <div class="sun">
         <div class="rays"></div>
     </div>
 </div>
-
 <div class="icon rainy">
     <div class="cloud"></div>
     <div class="rain"></div>
 </div>
-
 <div class="icon sun-cloudy">
     <div class="cloud"></div>
     <div class="sun">
         <div class="rays"></div>
     </div>
 </div>
-
 <div class="icon thunder-storm-sunny">
     <div class="cloud"></div>
     <div class="sun">
@@ -75,7 +104,6 @@
         <div class="bolt"></div>
     </div>
 </div>
-
 <div class="icon rainy">
     <div class="cloud"></div>
     <div class="rain"></div>
@@ -84,24 +112,20 @@
         <div class="bolt"></div>
     </div>
 </div>
-
 <div class="icon windy">
     <div class="wind"></div>
     <div class="wind"></div>
     <div class="wind"></div>
     <div class="wind"></div>
 </div>
-
 <div class="icon compass">
     <div class="compass_ring"></div>
     <div class="compass_key"></div>
 </div>
-
 <div class="icon rocket">
     <div class="rocket_thrust"></div>
     <div class="rocket_body"></div>
 </div>
-
 <div class="icon chrome">
     <div class="chrome_logo"></div>
 </div>
