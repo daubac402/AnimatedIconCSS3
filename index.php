1a889503921a87e5c8aac138fd010905fdbe763b
theanh <theanh@ilovex.co.jp>
2016-11-29 15:06:48 +0900

    add new ad units

--- index.php
+++ index.php
@@ -1,51 +1,94 @@
-<link rel="stylesheet" type="text/css" href="css/global.css?v=20161116">
-<script>
-(function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-19534192-5', 'auto');
-ga('send', 'pageview');
-</script>
-<div id="fb-root"></div>
-<script>
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: '330208494007688',
-        xfbml: true,
-        version: 'v2.8'
-    });
-};
+<head>
+    <meta name="description" content="All of Animated Icons here are made by css3, no images, no javascripts">
+    <meta name="keywords" content="animated icons, animated, icons, css3, images, google, analytic, adsense, weather, logo, online, javascript">
+    <meta name="author" content="daubac403@gmail.com">
 
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-</script>
+    <link rel="stylesheet" type="text/css" href="css/global.css?v=20161116">
+    <script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-19534192-5', 'auto');
+    ga('send', 'pageview');
+    </script>
+    <div id="fb-root"></div>
+    <script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '330208494007688',
+            xfbml: true,
+            version: 'v2.8'
+        });
+    };
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Animated Icons -->
-<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-8395820335361202" data-ad-slot="4553495373"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- animatedicons.pe.hu -->
+    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-8395820335361202" data-ad-slot="9996632977"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <script type='text/javascript'>
+    (function(w) {
+        if (navigator.userAgent.match(/iPhone|iPod|iPad|Android/i) == null) return;
+        var d = document,
+            h = d.getElementsByTagName('head')[0],
+            s = d.createElement('style'),
+            j = d.createElement('script'),
+            k = d.createElement('script');
+        s.setAttribute('rel', 'mw-page-block');
+        s.innerHTML = 'body > * {display:none !important}';
+        j.setAttribute('src', '//cdn.adsoptimal.com/advertisement/settings/49985.js');
+        k.setAttribute('src', '//cdn.adsoptimal.com/advertisement/dispatcher.js');
+        j.onerror = k.onerror = function() {
+            h.removeChild(s);
+            h.removeChild(j);
+            h.removeChild(k);
+        };
+        h.appendChild(s);
+        h.appendChild(j);
+        h.appendChild(k);
+    })(window);
+    </script>
+    <script type='text/javascript'>
+    (function(w) {
+        var d = document,
+            h = d.getElementsByTagName('head')[0],
+            j = d.createElement('script'),
+            k = d.createElement('script');
+        j.setAttribute('src', '//cdn.adsoptimal.com/advertisement/settings/49985.js');
+        k.setAttribute('src', '//cdn.adsoptimal.com/advertisement/manual.js');
+        h.appendChild(j);
+        h.appendChild(k);
+    })(window);
+    </script>
+    <title>Animated Icons</title>
+</head>
 
-<title>Animated Icons</title>
-<h1>Animated Icons</h1>
-<h2>
+<body>
+    <div style="width: 100%">
+        <div class="adsoptimal-slot" style="width: 728px; height: 90px; margin: 0 auto;"></div>
+    </div>
+    <h1>Animated Icons</h1>
+    <h2>
     <div
   class="fb-like"
   data-share="true"
@@ -56,84 +99,88 @@ window.fbAsyncInit = function() {
   >
 </div>
 </h2>
-<div class="icon sun-shower">
-    <div class="cloud"></div>
-    <div class="sun">
-        <div class="rays"></div>
+    <div class="icon sun-shower">
+        <div class="cloud"></div>
+        <div class="sun">
+            <div class="rays"></div>
+        </div>
+        <div class="rain"></div>
     </div>
-    <div class="rain"></div>
-</div>
-<div class="icon thunder-storm">
-    <div class="cloud"></div>
-    <div class="lightning">
-        <div class="bolt"></div>
-        <div class="bolt"></div>
+    <div class="icon thunder-storm">
+        <div class="cloud"></div>
+        <div class="lightning">
+            <div class="bolt"></div>
+            <div class="bolt"></div>
+        </div>
     </div>
-</div>
-<div class="icon cloudy">
-    <div class="cloud"></div>
-    <div class="cloud"></div>
-</div>
-<div class="icon eclipse">
-    <div class="moon_eclipse"></div>
-    <div class="sun sun-eclipse">
-        <div class="rays"></div>
+    <div class="icon cloudy">
+        <div class="cloud"></div>
+        <div class="cloud"></div>
     </div>
-</div>
-<div class="icon flurries">
-    <div class="cloud"></div>
-    <div class="snow">
-        <div class="flake"></div>
-        <div class="flake"></div>
+    <div class="icon eclipse">
+        <div class="moon_eclipse"></div>
+        <div class="sun sun-eclipse">
+            <div class="rays"></div>
+        </div>
     </div>
-</div>
-<div class="icon sunny">
-    <div class="sun">
-        <div class="rays"></div>
+    <div class="icon flurries">
+        <div class="cloud"></div>
+        <div class="snow">
+            <div class="flake"></div>
+            <div class="flake"></div>
+        </div>
     </div>
-</div>
-<div class="icon rainy">
-    <div class="cloud"></div>
-    <div class="rain"></div>
-</div>
-<div class="icon sun-cloudy">
-    <div class="cloud"></div>
-    <div class="sun">
-        <div class="rays"></div>
+    <div class="icon sunny">
+        <div class="sun">
+            <div class="rays"></div>
+        </div>
     </div>
-</div>
-<div class="icon thunder-storm-sunny">
-    <div class="cloud"></div>
-    <div class="sun">
-        <div class="rays"></div>
+    <div class="icon rainy">
+        <div class="cloud"></div>
+        <div class="rain"></div>
     </div>
-    <div class="lightning">
-        <div class="bolt"></div>
-        <div class="bolt"></div>
+    <div class="icon sun-cloudy">
+        <div class="cloud"></div>
+        <div class="sun">
+            <div class="rays"></div>
+        </div>
     </div>
-</div>
-<div class="icon rainy">
-    <div class="cloud"></div>
-    <div class="rain"></div>
-    <div class="lightning when_rainy">
-        <div class="bolt"></div>
-        <div class="bolt"></div>
+    <div class="icon thunder-storm-sunny">
+        <div class="cloud"></div>
+        <div class="sun">
+            <div class="rays"></div>
+        </div>
+        <div class="lightning">
+            <div class="bolt"></div>
+            <div class="bolt"></div>
+        </div>
     </div>
-</div>
-<div class="icon windy">
-    <div class="wind"></div>
-    <div class="wind"></div>
-    <div class="wind"></div>
-    <div class="wind"></div>
-</div>
-<div class="icon compass">
-    <div class="compass_ring"></div>
-    <div class="compass_key"></div>
-</div>
-<div class="icon rocket">
-    <div class="rocket_thrust"></div>
-    <div class="rocket_body"></div>
-</div>
-<div class="icon chrome">
-    <div class="chrome_logo"></div>
-</div>
+    <div class="icon rainy">
+        <div class="cloud"></div>
+        <div class="rain"></div>
+        <div class="lightning when_rainy">
+            <div class="bolt"></div>
+            <div class="bolt"></div>
+        </div>
+    </div>
+    <div class="icon windy">
+        <div class="wind"></div>
+        <div class="wind"></div>
+        <div class="wind"></div>
+        <div class="wind"></div>
+    </div>
+    <div class="icon compass">
+        <div class="compass_ring"></div>
+        <div class="compass_key"></div>
+    </div>
+    <div class="icon rocket">
+        <div class="rocket_thrust"></div>
+        <div class="rocket_body"></div>
+    </div>
+    <div class="icon chrome">
+        <div class="chrome_logo"></div>
+    </div>
+    <div style="width: 100%">
+        <div class="adsoptimal-slot" style="width: 728px; height: 90px; margin: 0 auto;"></div>
+    </div>
+</body>
